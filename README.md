4b5d63d56e8a76ef11cb1c8720e7f0551325b52b
Evan Cope <copev313@gmail.com>
2021-08-17 13:47:38 -0400

    finished + added some improvements

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Chatbot-Using-Deep-Learning
- We build a chatbot by implementing machine learning and natural language processing.
+ A chatbot that implements machine learning and natural language processing. We use Keras, Tensorflow and NLTK to recongize the user's intent and generate the appropriate response.
+
+## Getting Started
+ Create a virtual environment and install the requirements:
+ `pip install -r requirements.txt`
+
+ Train the model:
+ - Run all the code cells in the notebook 'trainer.ipynb'.
+
+ Run the application:
+ `python chat.py`
---
+++ chatbot/chat.py
@@ -0,0 +1,63 @@
+import json
+import pickle
+
+import colorama
+import numpy as np
+from tensorflow import keras
+
+colorama.init()
+from colorama import Fore, Style
+
+
+# Load the intents data:
+with open("intents.json") as json_file:
+    data = json.load(json_file)
+
+
+def chat():
+    # Load the trained model:
+    model = keras.models.load_model("chat_model")
+
+    # Load the tokenizer object:
+    with open("tokenizer.pickle", "rb") as handle:
+        tokenizer = pickle.load(handle)
+
+    # Load the label encoder object:
+    with open("label_encoder.pickle", "rb") as enc:
+        label_encoder = pickle.load(enc)
+
+    # Define parameters:
+    max_length = 20
+
+    # Event loop:
+    while True:
+        print(Fore.GREEN + "You: ", Style.RESET_ALL, end="")
+        user_input = input()
+
+        # Handle exit command:
+        if (user_input.lower() in ['quit', 'exit', 'stop']):
+            print(Fore.LIGHTBLUE_EX + "Chatbot: Bye bye!" + Style.RESET_ALL)
+            break
+
+        response = model.predict(keras.preprocessing.sequence.pad_sequences(
+                                    tokenizer.texts_to_sequences([user_input]),
+                                    maxlen=max_length,
+                                    truncating='post')
+                                 )
+        tag = label_encoder.inverse_transform([np.argmax(response)])
+        
+        for i in data['intents']:
+            if (i['tag'] == tag):
+                print(Fore.LIGHTBLUE_EX + "Chatbot:" + Style.RESET_ALL ,
+                      np.random.choice(i['responses']))
+
+
+# Our driver code:
+if __name__ == "__main__":
+    # Print the welcome message:
+    print(Fore.YELLOW + 
+          "You can begin messaging the bot. Type 'quit' to end the session." +
+          Style.RESET_ALL)
+
+    # Launch our chat function:
+    chat()
